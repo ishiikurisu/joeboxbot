47d8a6d78cef4ca994aa4edf7beed8961a31d91a
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-04-13 16:43:35 -0300

    Trying to play a song using VLC and Ruby

--- app/jukebox/Jukebox.rb
+++ app/jukebox/Jukebox.rb
@@ -4,11 +4,17 @@ class Jukebox
     def initialize where
         @where = where
         # TODO Load get directory contents
+        @songs = get_available_songs
     end
 
     def get_available_songs
         Dir.entries(@where).keep_if do |entry|
             (entry != '.') && (entry != '..')
         end
+    end
+
+    def play_nth_song n
+        cmd = "vlc \"#{@songs[n]}\""
+        `#{cmd}`
     end
 end
--- test/test.rb
+++ test/test.rb
@@ -10,10 +10,11 @@ class TestPerson < MiniTest::Test
         assert_equal 5, @jukebox.get_available_songs.length
     end
 
+    # TODO Check if song name is being correctly referenced
+
     def test_first_song
-        assert_nothing_raised do
-            songs = @jukebox.get_available_songs
-            @jukebox.play_nth_song 0
-        end
+        assert_empty @jukebox.play_nth_song 0
     end
+
+    # TODO Test playing many songs
 end
