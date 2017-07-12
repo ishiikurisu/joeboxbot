4e4a438a80ff05dc3d8e2faaebc36f7c31fa79a3
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-12 14:44:07 -0300

    Adding /help command

--- app/main.rb
+++ app/main.rb
@@ -1,36 +1,70 @@
 require 'telegram_bot'
 require './app/jukebox/Jukebox.rb'
 
-if __FILE__ == $PROGRAM_NAME
-  bot = TelegramBot.new token: ARGV[0]
-  jukebox = Jukebox.new ARGV[1]
-  jukebox.play_all_songs
-  begin
-    bot.get_updates do |message|
+class App
+  def initialize api, where
+    @bot = TelegramBot.new token: api
+    @jukebox = Jukebox.new where
+  end
+
+  def play
+    @jukebox.play_all_songs
+    @bot.get_updates do |message|
       puts "@#{message.from.username}: #{message.text}"
-      command = message.get_command_for bot
+      command = message.get_command_for @bot
 
       message.reply do |reply|
         case command
+        when /^\/start/i
+          reply.text = help
+        when /^\/help/i
+          reply.text = help
         when /^\/list/i
-          songs = [ ]
-          jukebox.songs.each_index do |i|
-            songs << "#{i+1}. #{jukebox.songs[i]}\n"
-          end
-          # BUG This message is not being sent
-          reply.text = songs.join
+          reply.text = list
         when /^\/add/i
           arguments = command.split " "
           index = arguments[1].to_i - 1
-          jukebox.add_to_playlist index
-          reply.text = "adding #{jukebox.generate_nth_song_name index} to the playlist..."
+          reply.text = add index
         else
           reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
         end
+
         puts "sending #{reply.text.inspect} to @#{message.from.username}"
-        reply.send_with bot
+        reply.send_with @bot
       end
     end
+  end
+
+  private
+  def add index
+    @jukebox.add_to_playlist index
+    return "adding #{jukebox.generate_nth_song_name index} to the playlist..."
+  end
+
+  def list
+    songs = [ ]
+    @jukebox.songs.each_index do |i|
+      songs << "#{i+1}. #{@jukebox.songs[i]}\n"
+    end
+    # BUG This message is not being sent
+    songs.join.to_s
+  end
+
+  def help
+    return <<-HELP
+# Welcome to the Joeboxbot!
+/list - Lists all available songs
+/add index - Adds a song to a playlist by its number
+/help displays this message
+    HELP
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  app = App.new ARGV[0], ARGV[1]
+  begin
+    app.play
   rescue Interrupt => e
     puts 'Program interrupted'
   end
